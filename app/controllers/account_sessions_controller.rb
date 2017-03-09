3ef08962995b578b62fcf5296d6e2c9bd3da0766
kazuhiro.ootani <administrator@user.local>
2017-03-10 06:58:47 +0900

    Create account_sessions

---
+++ app/controllers/account_sessions_controller.rb
@@ -0,0 +1,19 @@
+class AccountSessionsController < ApplicationController
+  def new
+    @account = Account.new
+  end
+
+  def create
+    if @account = login(params[:login], params[:password])
+      redirect_back_or_to(tasks_path, notice: 'ログインしました')
+    else
+      flash.now[:alert] = 'ログイン失敗'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(new_account_session_path, notice: 'ログアウトしました')
+  end
+end
