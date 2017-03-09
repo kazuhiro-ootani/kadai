3def07c79358523f6af9a83a8b47ba08cc04546a
kazuhiro.ootani <administrator@user.local>
2017-03-10 06:46:12 +0900

    Change application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,14 @@ Bundler.require(*Rails.groups)
 
 module KOtani
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+
+    config.generators do |g|
+      g.test_framework  nil
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
   end
 end
