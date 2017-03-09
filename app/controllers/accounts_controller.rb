f46c9c304f82991822f4370b1de5668b1b0c7922
kazuhiro.ootani <administrator@user.local>
2017-03-10 06:53:20 +0900

    Create Account

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,64 @@
+class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update, :destroy]
+
+  # GET /accounts
+  def index
+    @accounts = Account.all
+  end
+
+  # GET /accounts/1
+  def show
+  end
+
+  # GET /accounts/new
+  def new
+    @account = Account.new
+  end
+
+  # GET /accounts/1/edit
+  def edit
+  end
+
+  # POST /accounts
+  def create
+    @account = Account.new(account_params)
+
+    respond_to do |format|
+      if @account.save
+        format.html { redirect_to @account, notice: 'Account was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /accounts/1
+  def update
+    respond_to do |format|
+      if @account.update(account_params)
+        format.html { redirect_to @account, notice: 'Account was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /accounts/1
+  def destroy
+    @account.destroy
+    respond_to do |format|
+      format.html { redirect_to accounts_url, notice: 'Account was successfully destroyed.' }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_account
+      @account = Account.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def account_params
+      params.require(:account).permit(:login, :password, :password_confirmation)
+    end
+end
