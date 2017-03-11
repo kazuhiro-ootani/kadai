94869ea70ba0f6a2fbe8a3bcc77a410ae26c8316
kazuhiro.ootani <administrator@user.local>
2017-03-11 22:16:18 +0900

    Change for link and flash-message

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to tasks_path, notice: 'タスクを編集しました' }
       else
         format.html { render :edit }
       end
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to tasks_url, notice: 'タスクを削除しました.' }
     end
   end
 
