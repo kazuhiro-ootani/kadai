8c00a8b5e6bae87ee6f86173740bb66aa2ed8df3
kazuhiro.ootani <administrator@user.local>
2017-03-16 06:24:41 +0900

    Set selenium

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,6 +49,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include FactoryGirl::Syntax::Methods
+  Capybara.default_driver = :selenium
   config.include Capybara::DSL
   config.include Sorcery::TestHelpers::Rails::Controller, type: :controller
   config.include Sorcery::TestHelpers::Rails::Integration, type: :feature
